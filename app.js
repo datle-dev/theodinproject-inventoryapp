fac695647609c441e48e45bc2ee9f3af9ce7e00e
datle-dev <dle.dev@outlook.com>
2024-04-22 13:06:53 -0400

    Update router objects and routers used by app

--- app.js
+++ app.js
@@ -7,8 +7,7 @@ const logger = require('morgan');
 require('dotenv').config();
 
 const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
-const catalogRouter = require('./routes/catalog');
+const inventoryRouter = require('./routes/inventory');
 
 const app = express();
 
@@ -34,8 +33,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/catalog', catalogRouter);
+app.use('/inventory', inventoryRouter);
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
