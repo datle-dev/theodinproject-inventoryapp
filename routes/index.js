cd67e329dd2b3451ae6104683227405ecf5e46a1
datle-dev <dle.dev@outlook.com>
2024-04-22 13:05:47 -0400

    Update index to be inventory route

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
-/* GET home page. */
 router.get('/', (req, res, next) => {
-  res.redirect('/catalog');
+  res.redirect('/inventory');
 });
 
 module.exports = router;
