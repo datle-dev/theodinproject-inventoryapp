50e3057af4e8b7a0eafe7f5ee9a1368157386a99
datle-dev <dle.dev@outlook.com>
2024-04-23 10:01:49 -0400

    Add view of item details

--- controllers/itemController.js
+++ controllers/itemController.js
@@ -4,18 +4,18 @@ const Series = require('../models/series');
 const asyncHandler = require('express-async-handler');
 
 exports.index = asyncHandler(async (req, res, next) => {
-    const [numItems, numCategories, numSeries] = await Promise.all([
-        Item.countDocuments({}).exec(),
-        Category.countDocuments({}).exec(),
-        Series.countDocuments({}).exec(),
-    ]);
+  const [numItems, numCategories, numSeries] = await Promise.all([
+    Item.countDocuments({}).exec(),
+    Category.countDocuments({}).exec(),
+    Series.countDocuments({}).exec(),
+  ]);
 
-    res.render('pages/index', {
-        title: 'Inventory Home',
-        item_count: numItems,
-        category_count: numCategories,
-        series_count: numSeries,
-    });
+  res.render('pages/index', {
+    title: 'Inventory Home',
+    item_count: numItems,
+    category_count: numCategories,
+    series_count: numSeries,
+  });
 });
 
 exports.item_list = asyncHandler(async (req, res, next) => {
@@ -32,29 +32,37 @@ exports.item_list = asyncHandler(async (req, res, next) => {
 });
 
 exports.item_detail = asyncHandler(async (req, res, next) => {
-    res.send(`not implemented: item detail: ${req.params.id}`);
+  const item = await Item.findById(req.params.id)
+    .populate('category')
+    .populate('series')
+    .exec();
+
+  res.render('pages/itemDetail', {
+    title: 'Item Detail',
+    item: item,
+  });
 });
 
 exports.item_create_get = asyncHandler(async (req, res, next) => {
-    res.send('not implemented: item create get');
+  res.send('not implemented: item create get');
 });
 
 exports.item_create_post = asyncHandler(async (req, res, next) => {
-    res.send('not implemented: item create post');
+  res.send('not implemented: item create post');
 });
 
 exports.item_update_get = asyncHandler(async (req, res, next) => {
-    res.send('not implemented: item update get');
+  res.send('not implemented: item update get');
 });
 
 exports.item_update_post = asyncHandler(async (req, res, next) => {
-    res.send('not implemented: item update post');
+  res.send('not implemented: item update post');
 });
 
 exports.item_delete_get = asyncHandler(async (req, res, next) => {
-    res.send('not implemented: item delete get');
+  res.send('not implemented: item delete get');
 });
 
 exports.item_delete_post = asyncHandler(async (req, res, next) => {
-    res.send('not implemented: item delete post');
+  res.send('not implemented: item delete post');
 });
